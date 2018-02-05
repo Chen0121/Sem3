14c3049117e28d17879e022b7ac0a493f9382a84
HelloWorldddddd <kiirochen@gmail.com>
2018-02-05 11:18:49 -0500

    lab4 temporarily commit

--- app/src/main/java/com/example/chen/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/chen/androidlabs/LoginActivity.java
@@ -16,19 +16,21 @@ public class LoginActivity extends Activity {
     protected static final String ACTIVITY_NAME = "LoginActivity";
     EditText email;
     Button button;
+    SharedPreferences sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Log.i(ACTIVITY_NAME, "In onCreate()");
 
-        email=(EditText) findViewById(R.id.login_name);
-        final SharedPreferences sharedPref = getSharedPreferences(
+        email = (EditText) findViewById(R.id.login_name);
+        sharedPref = getSharedPreferences(
                 getString(R.string.app_name), Context.MODE_PRIVATE);
-        String text = sharedPref.getString(getString(R.string.loginname),"email@domain.com");
+        String text = sharedPref.getString(getString(R.string.loginname), "email@domain.com");
         email.setText(text);
 
-        button = (Button)findViewById(R.id.button_login);
+        button = (Button) findViewById(R.id.button_login);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,12 +39,12 @@ public class LoginActivity extends Activity {
                 editor.putString(getString(R.string.loginname), email.getText().toString());
                 editor.commit();
 
+
                 Intent start = new Intent(LoginActivity.this, StartActivity.class);
                 startActivity(start);
             }
         });
-
-    }
+}
 
     @Override
     protected void onResume(){
