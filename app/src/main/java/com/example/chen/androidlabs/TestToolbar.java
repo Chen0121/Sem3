d5a4b4f500cd6285c67b22751796d8930977dea1
HelloWorldddddd <kiirochen@gmail.com>
2018-04-01 15:25:21 -0400

    lab8

--- app/src/main/java/com/example/chen/androidlabs/TestToolbar.java
+++ app/src/main/java/com/example/chen/androidlabs/TestToolbar.java
@@ -1,7 +1,10 @@
 package com.example.chen.androidlabs;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TestToolbar extends Activity {
 
@@ -9,6 +12,32 @@ public class TestToolbar extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_toolbar);
+    }
+
+    //create toolbar by inflating it from xml file
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater.inflate(R.menu.toolbar_menu, m);
+        return true;
+    }
+
+    //response items being selecetd
+    public boolean onOptionsItemSelected(MenuItem mi){
+        int id= mi.getItemId();
+
+        switch (id) {
+            case R.id.option_one:
+                Log.d("Toolbar","Option 1 selected");
+                break;
+            case R.id.option_two:
+                Log.d("Toolbar","Option 2 selected");
+                break;
+            case R.id.option_three:
+                Log.d("Toolbar","Option 3 selected");
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
